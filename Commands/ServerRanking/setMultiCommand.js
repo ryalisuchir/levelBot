16b6f07beda0efa676119d35b7d2028e113feffe
SharkDjokovic <93061471+SharkDjokovic@users.noreply.github.com>
2022-06-08 19:04:55 -0500

    Create setMultiCommand.js

---
+++ Commands/ServerRanking/setMultiCommand.js
@@ -0,0 +1,75 @@
+let multi = require(`../../schemas/multi`);
+const { MessageEmbed, Permissions } = require("discord.js");
+
+module.exports = {
+  name: "setMultiplier",
+  aliases: ["sm", "setmulti"],
+  description: "Change the multiplier for a guild.",
+  timeout: 2,
+  /**
+   *
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+    if (!message.member.permissions.has([Permissions.FLAGS.ADMINISTRATOR]))
+      return;
+
+    let serverProfile;
+    try {
+      serverProfile = await multi.findOne({
+        guildID: message.guild.id,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    if (!serverProfile) {
+      serverProfile = new multi({
+        guildID: message.guild.id,
+        multi: 1,
+      });
+      serverProfile.save();
+    }
+    console.log(args[0]);
+    if (!args[0])
+      return message.reply({
+        content: "Specify a valid multiplier level (must be an integer).",
+      });
+
+    function isPositiveInteger(str) {
+      if (typeof str !== "string") {
+        return false;
+      }
+
+      const num = Number(str);
+
+      if (Number.isInteger(num) && num > 0) {
+        return true;
+      }
+
+      return false;
+    }
+    if (!isPositiveInteger(args[0]))
+      return message.reply({
+        content: `Specify a valid multiplier level, that is a positive integer.`,
+      });
+
+    let newMulti = parseInt(args[0]);
+
+    serverProfile.multi = newMulti;
+    serverProfile.save();
+
+    message.reply({
+      embeds: [
+        new MessageEmbed()
+          .setDescription(
+            `Successfully changed the guild multi to: ${newMulti}%.`
+          )
+          .setColor("303136"),
+      ],
+    });
+  },
+};
+
+//Credits: Shark#2538 for coding.
