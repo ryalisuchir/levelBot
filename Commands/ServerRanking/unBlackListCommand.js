ff8355444d94458d7451d7c2e4970910e0278e5c
SharkDjokovic <93061471+SharkDjokovic@users.noreply.github.com>
2022-06-08 19:05:07 -0500

    Create unBlackListCommand.js

---
+++ Commands/ServerRanking/unBlackListCommand.js
@@ -0,0 +1,54 @@
+let blacklist = require(`../../schemas/multi`);
+const { MessageEmbed, Permissions } = require("discord.js");
+
+module.exports = {
+  name: "unBlackList",
+  aliases: ["unbl", "unblacklist"],
+  description: "UnBlacklists a user from gaining XP in a specific guild.",
+  timeout: 2,
+  /**
+   *
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+    if (!message.member.permissions.has([Permissions.FLAGS.ADMINISTRATOR]))
+      return;
+
+    const User =
+      message.mentions.members.first() ||
+      message.guild.members.cache.find(
+        (member) =>
+          member.user.username.toLowerCase() === args.join(" ").toLowerCase()
+      ) ||
+      message.guild.members.cache.get(args[0]) ||
+      message.guild.members.cache.find(
+        (member) =>
+          member.displayName.toLowerCase() === args.join(" ").toLowerCase()
+      ) ||
+      message.member;
+
+    if (User.id == message.author.id)
+      return message.reply({
+        content: "You cannot remove a blacklist from yourself.",
+      });
+
+    blacklist.findOne({ id: User.user.id }, async (err, data) => {
+      if (err) throw err;
+      if (data) {
+        await blacklist
+          .findOneAndDelete({ id: User.user.id })
+          .catch((err) => console.log(err));
+        const unblacklistEmbed = new MessageEmbed()
+          .setColor(color)
+          .setDescription(
+            `${User.user.tag} has been removed from a blacklist.`
+          );
+        message.channel.send({ embeds: [unblacklistEmbed] });
+      } else {
+        message.reply(`${User.user.tag} is not currently blacklisted.`);
+      }
+    });
+  },
+};
